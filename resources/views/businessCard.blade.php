b56e3a93c60489f9cf7a55e5ada2a4a6494cc5f2
Ivan Belousov <vanek@email.ru>
2024-04-02 10:30:39 +0500

    изменил названия страниц на Study Haven

--- resources/views/businessCard.blade.php
+++ resources/views/businessCard.blade.php
@@ -4,7 +4,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Study Haven</title>
     <link rel="stylesheet" href="\css\base_style.css">
     <link rel="stylesheet" href="\css\business_card.css">
 </head>
